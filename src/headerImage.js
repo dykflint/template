ff0f670e2dd6092be2cf4f2ca990610f0be446cb
Konstantin <77123607+dykflint@users.noreply.github.com>
2024-02-28 05:34:35 +0100

    Create template

--- src/headerImage.js
+++
@@ -1,7 +0,0 @@
-import TodoerSVG from './images/Todoer.png';
-const header = document.querySelector('#header');
-const headerImage = new Image();
-headerImage.src = TodoerSVG;
-header.appendChild(headerImage);
-
-export default header;
